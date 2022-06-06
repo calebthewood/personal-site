ff755661e4f89e87d10d831cb4526dc078075681
calebthewood <calebwood.cs@gmail.com>
2022-06-06 08:33:42 -0700

    Tweak CSS, add JS todos

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 "use strict";
 
-
+/***************************************************** Carousel Functionality */
 const myCarousel = document.querySelector('#myCarousel');
 const carousel = new bootstrap.Carousel(myCarousel, {
   interval: 2000,
@@ -14,3 +14,10 @@ const prev = document.querySelector(".carousel-control-prev");
 
 prev.click(() => carousel.carousel("prev"));
 next.click(() => carousel.carousel("next"));
+
+/*
+TODO:
+  1. add dynamic date to copyright
+  2. expand projects to include 'fun' projects'
+  3. add links for twitter and angel list
+*/
